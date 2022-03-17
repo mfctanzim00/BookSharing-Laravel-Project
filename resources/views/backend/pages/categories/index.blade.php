0464b909377415cb6aa1d5eac3eb351a143831aa
mfctanzim00 <69580317+mfctanzim00@users.noreply.github.com>
2022-03-17 13:35:41 +0600

    Update index.blade.php

--- resources/views/backend/pages/categories/index.blade.php
+++ resources/views/backend/pages/categories/index.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.app')
-
 @section('admin-content')
+
 <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Manage Categories</h1>
